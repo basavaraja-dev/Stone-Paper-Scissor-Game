dc1658c9278c980bfd24bc2a8c4f596c2ca7509d
Basavaraja D Nayaka <basavarajdnayaka@gmail.com>
2026-06-20 12:48:37 +0530

    updated

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ while True:
 
     print("welcome to Stone Paper Scissor game!!🎮🚀")
 
-    print("only three attempts are possible😁")
+    print("only five attempts are possible😁")
 
     choices=["stone", "paper" ,"scissor"]
 
@@ -13,18 +13,18 @@ while True:
 
     attempts=1
 
-    while attempts<=3:
+    while attempts<=5:
 
         computer_choice=random.choice(choices)
 
         user_choice=input(f"attempt{attempts} - enter your choice:").lower()
 
         if user_choice not in choices:
-            print("enter valid name😁!!")
+            print("enter valid choice😁!!")
             break
 
-        print(f"your choice is(👨🏻) -{user_choice}")
-        print(f"computer choice is(🖥️) -{computer_choice}")
+        print(f"your choice is - {user_choice}")
+        print(f"computer choice is - {computer_choice}")
 
         if user_choice==computer_choice:
             print("tie!!😉")
@@ -32,28 +32,33 @@ while True:
         elif user_choice=="stone" and computer_choice=="scissor":
             print("you won!!🏆")
             user_score+=10
-            break
         elif user_choice=="paper" and computer_choice=="stone":
             print("you won!!🏆")
             user_score+=10
-            break
         elif user_choice=="scissor" and computer_choice=="paper":
             print("you won!!🏆")
             user_score+=10
-            break
 
         else:
-            print("you lose , bad luck!!🙁")
+            print("you lose!!🙁")
             computer_score+=10
 
         attempts+=1
 
-    if attempts>3:
+    if attempts>5:
         print("game over!!❌😩")
 
-    print(f"your final score is(👨🏻) - {user_score} 🎉")
-    print(f"computer final score is(🖥️) - {computer_score} 🎉")
+    print(f"your final score is - {user_score} 🎉")
+    print(f"computer final score is - {computer_score} 🎉")
 
+    if user_score>computer_score:
+        print("Congratulations!! you have won the game!🎉🏆")
+    elif user_score==computer_score:
+        print("good try!!the game has been tied up!👌")
+    else:
+        print("You lost!! better luck next time!😊")    
+
+    print("")
 
     print("You want to play again? 😇(yes/no)") 
     if input().lower()=="yes":  
@@ -61,7 +66,6 @@ while True:
     else:
         print("Thanks for playing!!✌️")
         break
-
 
 
 
